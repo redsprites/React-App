fbfb6f1c79eba876273c0be3e1f18b3d7c5f3e42
redsprites <71545773+redsprites@users.noreply.github.com>
2023-01-22 00:21:38 -0500

    new update

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
+const API_URL = 'http://www.omdbapi.com?apikey=97887c4e';
+
+const App = () => {
+
+    const[searchTerm, setSearchTerm] =useState('');
+
+    const[movies, setMovies] = useState([]);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+    
+    return (
+        <div className="app">
+            <h1> MovieHub </h1>
+            <div className="search">
+                <input placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) =>setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="Search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (<MovieCard movie={movie} />))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+            )
+            }
+        </div>
+
+    );
+}
+
+export default App;
